633e807447c22790a1a4c7fe0af4c898d9af3286
Maks9m <maxim.kramarenko8a@gmail.com>
2025-01-11 16:40:10 +0200

    Removed code that's never read and improved naming

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,21 +2,18 @@
 // Sum all numbers from an array
 'use strict'
 
-const sum = function (s = () => {}) {
-  const sum = [0];
-  let k = 5;
-  for (const i of s) {
-    let t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
+const sum = function (arr = () => {}) {
+  const sumResolts = [0];
+  for (const element of arr) {
+    const type = typeof element;
+    if (type === 'number') {
+      if (sumResolts.length > 0) {
+        const newSum = sumResolts[sumResolts.length - 1] + element;
+        sumResolts.push(newSum);
       }
     }
   }
-  sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
+  return sumResolts[sumResolts.length - 1];
 };
 
 module.exports = sum;
