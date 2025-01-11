497f56ee015828a360e3a13df3fcfd9a72cb9365
Maks9m <maxim.kramarenko8a@gmail.com>
2025-01-11 16:47:03 +0200

    Changed array of sumResults to a variable to simlify logic

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,19 +1,15 @@
 // Refactor following solution
 // Sum all numbers from an array
-'use strict'
+'use strict';
 
 const sum = (arr) => {
-  const sumResolts = [0];
+  let sumResolt = 0;
   for (const element of arr) {
-    const type = typeof element;
-    if (type === 'number') {
-      if (sumResolts.length > 0) {
-        const newSum = sumResolts[sumResolts.length - 1] + element;
-        sumResolts.push(newSum);
-      }
+    if (typeof element === 'number') {
+      sumResolt += element;
     }
   }
-  return sumResolts[sumResolts.length - 1];
+  return sumResolt;
 };
 
 module.exports = sum;
